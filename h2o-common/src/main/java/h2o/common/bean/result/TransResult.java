0bc2f84809936bf555ab90289a1fa53318fa10fe
sixshot626 <sixshot626@gmail.com>
2017-06-09 18:26:11 +0800

    add

--- h2o-common/src/main/java/h2o/common/bean/result/TransResult.java
+++ h2o-common/src/main/java/h2o/common/bean/result/TransResult.java
@@ -5,57 +5,15 @@ import java.io.Serializable;
 /**
  * Created by zhangjianwei on 2017/6/7.
  */
-public class TransResult implements Serializable {
+public class TransResult extends TransStatus implements Serializable {
 
     private static final long serialVersionUID = -1448116426261181686L;
 
-    private int status = -1;
-
-    private boolean isFinal;
-
-    private String code;
-
-    private String msg;
 
     private Object result;
 
     private Throwable e;
 
-    public int getStatus() {
-        return status;
-    }
-
-    public TransResult setStatus(int status) {
-        this.status = status;
-        return this;
-    }
-
-    public boolean isFinal() {
-        return isFinal;
-    }
-
-    public TransResult setFinal(boolean aFinal) {
-        isFinal = aFinal;
-        return this;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public TransResult setCode(String code) {
-        this.code = code;
-        return this;
-    }
-
-    public String getMsg() {
-        return msg;
-    }
-
-    public TransResult setMsg(String msg) {
-        this.msg = msg;
-        return this;
-    }
 
     public <R> R getResult() {
         return (R)result;
@@ -74,4 +32,5 @@ public class TransResult implements Serializable {
         this.e = e;
         return this;
     }
+
 }
---
+++ h2o-common/src/main/java/h2o/common/bean/result/TransStatus.java
@@ -0,0 +1,58 @@
+package h2o.common.bean.result;
+
+import java.io.Serializable;
+
+/**
+ * Created by zhangjianwei on 2017/6/7.
+ */
+public class TransStatus implements Serializable {
+
+    private static final long serialVersionUID = -8626841949944329920L;
+
+    private int status = -1;
+
+    private boolean isFinal;
+
+    private String code;
+
+    private String msg;
+
+
+    public int getStatus() {
+        return status;
+    }
+
+    public TransStatus setStatus(int status) {
+        this.status = status;
+        return this;
+    }
+
+    public boolean isFinal() {
+        return isFinal;
+    }
+
+    public TransStatus setFinal(boolean aFinal) {
+        isFinal = aFinal;
+        return this;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public TransStatus setCode(String code) {
+        this.code = code;
+        return this;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public TransStatus setMsg(String msg) {
+        this.msg = msg;
+        return this;
+    }
+
+
+}
