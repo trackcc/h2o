e4a46d1a25054722a655fd6e2f46b5e8c6739cba
sixshot626 <sixshot626@gmail.com>
2017-07-02 19:55:56 +0800

    行锁

--- h2o-dao/src/main/java/h2o/dao/advanced/DaoBasicUtil.java
+++ h2o-dao/src/main/java/h2o/dao/advanced/DaoBasicUtil.java
@@ -1,5 +1,6 @@
 package h2o.dao.advanced;
 
+import h2o.common.util.lang.StringUtil;
 import h2o.dao.Dao;
 import h2o.dao.DbUtil;
 
@@ -42,7 +43,23 @@ public final class DaoBasicUtil {
     }
 
     public <T extends AbstractEntity> T get( T entity ) {
-        return (T)dao.get( entity.getClass() , "select * from " + entity.getTableName() +  " where " + entity.getW() , entity );
+        return this.get( entity , true );
+    }
+
+    public <T extends AbstractEntity> T getAndLock( T entity ) {
+        return this.get( entity , true );
+    }
+
+    public <T extends AbstractEntity> T get( T entity , boolean lock ) {
+
+        StringBuilder sql = new StringBuilder();
+
+        StringUtil.append( sql , "select * from " , entity.getTableName() ,  " where " , entity.getW() );
+        if( lock ) {
+            sql.append(" for update ");
+        }
+
+        return (T)dao.get( entity.getClass() , sql.toString() , entity );
     }
 
 }
