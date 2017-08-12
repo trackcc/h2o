9e35f6f990c0b852384c7167f6daf393eae9a0ec
sixshot626 <sixshot626@gmail.com>
2017-08-12 18:40:16 +0800

    修改 Var 内部变量名为 v

--- h2o-common/src/main/java/h2o/common/util/lang/Var.java
+++ h2o-common/src/main/java/h2o/common/util/lang/Var.java
@@ -3,32 +3,32 @@ package h2o.common.util.lang;
 public class Var<T> implements java.io.Serializable {	
 
 	private static final long serialVersionUID = -8028944252330143592L;
-	
-	private volatile T var;
-	
-	public Var() {}
-	
-	public Var( T var ) {
-		this.var = var;
-	}
 
-	public T getVar() {
-		return var;
-	}
+	public volatile T v;
 
-	public void setVar(T var) {
-		this.var = var;
-	}
+    public Var() {
+    }
 
-	@Override
+    public Var(T v) {
+        this.v = v;
+    }
+
+    public T getV() {
+        return v;
+    }
+
+    public void setV(T v) {
+        this.v = v;
+    }
+
+    @Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((var == null) ? 0 : var.hashCode());
+		result = prime * result + ((v == null) ? 0 : v.hashCode());
 		return result;
 	}
 
-	@SuppressWarnings("rawtypes")
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -38,17 +38,17 @@ public class Var<T> implements java.io.Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Var other = (Var) obj;
-		if (var == null) {
-			if (other.var != null)
+		if (v == null) {
+			if (other.v != null)
 				return false;
-		} else if (!var.equals(other.var))
+		} else if (!v.equals(other.v))
 			return false;
 		return true;
 	}
 
 	@Override
 	public String toString() {
-		return var == null ? null : var.toString();
+		return v == null ? null : v.toString();
 	}
 	
 	
