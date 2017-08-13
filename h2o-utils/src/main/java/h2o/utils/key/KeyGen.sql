4bd7e7f09cdf8182567227c27165fa9337417339
sixshot626 <sixshot626@gmail.com>
2017-08-14 01:54:20 +0800

    fix bug: KeyGen sql 错误

--- h2o-utils/src/main/java/h2o/utils/key/KeyGen.sql
+++ h2o-utils/src/main/java/h2o/utils/key/KeyGen.sql
@@ -1,3 +1,3 @@
 :{selseq=select cyclicspace ,  seqno from sys_cycseq where seqobj = :seqobj for update };
-:{insseq=insert into sys_cycseq ( seqobj , cyclicspace ,  seqno ) values ( :seqobj , :cyclicspace , "0" ) };
+:{insseq=insert into sys_cycseq ( seqobj , cyclicspace ,  seqno ) values ( :seqobj , :cyclicspace , '0' ) };
 :{updseq=update sys_cycseq set seqno = :seqno , cyclicspace = :cyclicspace  where seqobj = :seqobj };
