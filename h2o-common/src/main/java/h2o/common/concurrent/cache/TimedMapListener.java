d72cb8368a70b06580cb0aa23627ff21644eccb2
sixshot626 <sixshot626@gmail.com>
2018-08-03 11:21:23 +0800

    清理

--- h2o-common/src/main/java/h2o/common/concurrent/cache/TimedMapListener.java
+++
@@ -1,9 +0,0 @@
-package h2o.common.concurrent.cache;
-
-public interface TimedMapListener {
-	
-	boolean onPutNewValue(Object key, Object value, final Object newValue);
-	
-	void onRemoved(Object key, Object value);
-
-}
